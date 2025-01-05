a68b8409441dfea8305f660be29e207e4083c83b
Rhon <rhonumahon@gmail.com>
2025-01-05 09:34:22 +0800

    Update environment path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rawn-cafe-backend-631bf37fe97e.herokuapp.com'
+  apiUrl: 'https://rawn-cafe-backend-631bf37fe97e.herokuapp.com/api'
 } as { production: boolean, apiUrl: string};
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
     production: false, // Staging is not the final production environment
-    apiUrl: 'https://rawn-cafe-backend-631bf37fe97e.herokuapp.com'  // Replace with your staging API URL
+    apiUrl: 'https://rawn-cafe-backend-631bf37fe97e.herokuapp.com/api'  // Replace with your staging API URL
   } as { production: boolean, apiUrl: string};
