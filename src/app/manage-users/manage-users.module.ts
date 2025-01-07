63bd91a38223f1f1178208af46f60feb76c88cd5
Rhon <rhonumahon@gmail.com>
2025-01-08 07:53:47 +0800

    FEAT: Add manage-users component

---
+++ src/app/manage-users/manage-users.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ManageUsersComponent } from './manage-users.component';
+import { RoleGuard } from '../guards/role.guard';
+import { AuthGuard } from '../guards/auth.guard';
+import { ROLES } from '../constants/roles.constants';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ManageUsersComponent,
+    canActivate: [RoleGuard, AuthGuard],
+    data: { role: [ROLES.User, ROLES.Admin, ROLES.SuperAdmin] }
+  }
+];
+
+@NgModule({
+  declarations: [ManageUsersComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ManageUsersModule { }
