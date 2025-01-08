60e00a970af5740a0eecb41378f1724a6e9e00f2
Rhon <rhonumahon@gmail.com>
2025-01-08 10:02:50 +0800

    FIX: Navigate the user when token expires

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ROLES } from './constants/roles.constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'rawncafe-web';
@@ -14,15 +15,20 @@ export class AppComponent implements OnInit, OnDestroy {
   isLoading: boolean = false;
   authId: string | null = '';
   authSubscription!: Subscription;
+  isAdmin: boolean | null = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private cdRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     // Subscribe to authentication state changes
     this.authSubscription = this.authService.isAuthenticated$.subscribe(isAuthenticated => {
       this.isAuthenticated = isAuthenticated;
-      this.authId = this.isAuthenticated ? this.authService.getUserId() : '';
-       if(!this.authId) this.router.navigate(['/login']);
+      setTimeout(() => {
+        this.authId = this.isAuthenticated ? this.authService.getUserId() : '';
+        const role = this.authService.getUserRole()
+        this.isAdmin = role && role !== ROLES.User
+        if(!this.authId) this.logout();
+      }, 3000)
     });
   }
 
